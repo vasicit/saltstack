34a6e01b5f4e77d6baad5c095b56b846b9d6f1d1
Vladimir Vasic <vladimir@vasic.us>
2017-11-30 12:17:15 -0800

    v1.0

--- change-salt-minion-id.sh
+++ change-salt-minion-id.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-sudo sed -i 's/.*^id: *.*/id: srv-ubt-web-vasicio/g' /etc/salt/minion
-systemctl restart salt-minion
-service salt-minion restart
-/etc/init.d/salt-mainion restart
+NEWNAME=$1
+sudo sed -i 's/.*^id: *.*/id: $NEWNAME/g' /etc/salt/minion
+sudo systemctl restart salt-minion
+sudo service salt-minion restart
+sudo /etc/init.d/salt-mainion restart
